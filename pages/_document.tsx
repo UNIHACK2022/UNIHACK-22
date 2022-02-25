e13006aaf511bac15930358a406abffe3b55d03e
Jeffrey Yao <33971845+jeffreydyao@users.noreply.github.com>
2022-02-25 11:15:48 +0000

    Added map search

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,5 +1,6 @@
 import NextDocument, { Html, Head, Main, NextScript } from "next/document";
 import React from "react";
+import Script from "next/script";
 type Props = {};
 
 class Document extends NextDocument<Props> {
@@ -16,6 +17,11 @@ class Document extends NextDocument<Props> {
             href="https://api.mapbox.com/mapbox-gl-js/v2.3.1/mapbox-gl.css"
             rel="stylesheet"
           />
+          <link
+            href="https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-geocoder/v4.7.2/mapbox-gl-geocoder.css"
+            rel="stylesheet"
+          />
+         <script src="https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-geocoder/v4.7.2/mapbox-gl-geocoder.min.js"></script>
         </Head>
         <body>
           <Main />
