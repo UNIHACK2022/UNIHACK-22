717283fc7fc8b21fa5bd20cab5e1af2ad61d9ab1
Jeffrey Yao <33971845+jeffreydyao@users.noreply.github.com>
2022-02-25 10:29:06 +0000

    Added Mapbox GL.JS

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -12,6 +12,10 @@ class Document extends NextDocument<Props> {
             href="https://rsms.me/inter/inter.css"
             as="font"
           />
+          <link
+            href="https://api.mapbox.com/mapbox-gl-js/v2.3.1/mapbox-gl.css"
+            rel="stylesheet"
+          />
         </Head>
         <body>
           <Main />
